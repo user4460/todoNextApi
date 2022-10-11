2fa481a546341fbcb7a7a56ab4f09cb568ed9849
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-11 12:21:51 +0800

    docker-compose

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //reactStrictModeとは、ReactのStrictモードを有効にするかどうかを指定する設定です。
   reactStrictMode: true,
+  //swcMinifyとは、SWCを使用してコードを最小化するかどうかを指定する設定です。
   swcMinify: true,
 }
 
+//exportsなのでnode.jsのモジュールとしてエクスポートする
 module.exports = nextConfig
