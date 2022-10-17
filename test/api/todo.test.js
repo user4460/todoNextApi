3e3924b1f629326c0647081d33258e55f3921fa8
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-17 23:08:41 +0800

    test

---
+++ test/api/todo.test.js
@@ -0,0 +1,36 @@
+
+describe("todoを作成する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoを編集する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoを削除する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoをチェックする", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
