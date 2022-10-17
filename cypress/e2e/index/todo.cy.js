3b99098de661dea4c58b7625a550b14427069434
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-17 11:33:35 +0800

    todo作成する

--- cypress/e2e/index/todo.cy.js
+++ cypress/e2e/index/todo.cy.js
@@ -27,7 +27,34 @@ describe("Authentication test in index page", () => {
    });
 });
 
-describe("Authentication test in index page", () => {
+describe("todoを作成する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoを編集する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoを削除する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoをチェックする", () => {
    const email = "test@example.com";
    const password = "123456";
 
